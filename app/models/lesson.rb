ef5267f24e28e6d6f0cee75ade77ab55c1e03e78
Brunno Pleffken <brunno.pleffken@outlook.com>
2021-05-17 11:41:46 -0300

    Parameterize

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,7 @@
 class Lesson < ApplicationRecord
   has_many :activities
+
+  def to_param
+    [id, title.parameterize].join('-')
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,8 @@ class Student < ApplicationRecord
   belongs_to :classroom
 
   has_many :activities
+
+  def to_param
+    [id, name.parameterize].join('-')
+  end
 end
